bede82fc5c0c1d6a17b16027323305674d3af84b
Gar Morley <garmorley@gmail.com>
2012-08-02 21:13:01 +0100

    Only redirect when flag is clicked on

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 chrome.extension.onRequest.addListener(function(request, sender) {
-  chrome.tabs.update(sender.tab.id, {url: request.redirect});
+  chrome.pageAction.onClicked.addListener(function(tab) {
+    chrome.tabs.update(tab.id, {url: request.redirect});
+  });
+  chrome.pageAction.show(sender.tab.id);
 });
